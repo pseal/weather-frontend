09fa99af2b92eccc7db851ae33c2642e0682b6aa
Prasenjit Seal <33536426+pseal@users.noreply.github.com>
2026-05-06 13:01:51 +0300

    Update app.js

--- app.js
+++ app.js
@@ -145,8 +145,7 @@ async function getWeather() {
 async function fetchAndRender(url) {
     try {
         const response = await fetch(url);
-        const data = await response.json();
-        
+        const data = await response.json();        
         lastWeatherData = data;
 
         const hours = data.next12Hours || [];
